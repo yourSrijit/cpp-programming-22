e1d34be9a3e6a81d4290d4d5e9ef2dc62e4c543f
Srijit Bera <berasrijit02@gmail.com>
2023-02-09 22:45:40 +0530

    Add files via upload

---
+++ 2_cin.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int age;
+cout <<"Enter your Age :";  /* << is Insertion Operator */
+cin >> age;                 /* >> is Extraction Operator */
+cout <<"Your age is :" <<age <<endl;
+}
